ca57326cd84ca102a7bcaac76e504ce9d88fc4e8
root <favourfreshboy@gmail.com>
2024-03-28 01:20:49 +0000

    all task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,8 +3,8 @@
 
 
 def find_peak(list_of _integers):
-    """Finds a peak in a list of unsorted integers."""
-    if list of_ integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_ integers[0]
+"""Finds a peak in a list of unsorted integers."""
+if list of_ integers:
+list_of_integers.sort(reverse=True)
+return list_of_ integers[0]
 
